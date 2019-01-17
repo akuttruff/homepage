db7263273645e8af260fe82e9edae335c4d2f48b
Amie Kuttruff <amie.kuttruff@skyward.io>
2019-01-16 19:04:04 -0800

    explicitly references images; removes google fonts CDN

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
-import HomePage from './components/app'
+import './fonts/Lato-Light.ttf';
+import './style.css';
+import HomePage from './components/app';
 
 render(<HomePage/>, document.getElementById('root'));
